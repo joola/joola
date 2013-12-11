f6a6c59522f744e5b15579ce1984c783d879deee
itayw <itay@joo.la>
2013-12-11 16:54:26 +0200

    Initial users dispatch and tests, relates to issue #67 - Added users route - Added api/users.spec to tests - Added delete to datasource dispatch - Fixed some config module issues - Fixed datasource related tests

--- test/unit/2_api/datasources.spec.js
+++ test/unit/2_api/datasources.spec.js
@@ -10,40 +10,42 @@
 
 
 describe("api-datasources", function () {
-  it("should return a valid list of data sources", function (done) {
-    joola.dispatch.datasources.list(function (err) {
-      return done(err);
-    });
-  });
+	it("should return a valid list of data sources", function (done) {
+		joola.dispatch.datasources.list(function (err) {
+			return done(err);
+		});
+	});
 
-  it("should add a data source", function (done) {
-    joola.dispatch.datasources.add({name: 'testSuite-api', type: 'test', _connectionString: 'test'}, function (err, datasource) {
-      expect(datasource).to.be.ok;
-      done(err);
-    });
-  });
+	it("should add a data source", function (done) {
+		joola.dispatch.datasources.add({name: 'testSuite-api', type: 'test', _connectionString: 'test'}, function (err, datasource) {
+			expect(datasource).to.be.ok;
+			done(err);
+		});
+	});
 
-  it("should update a data source", function (done) {
-    joola.dispatch.datasources.update({name: 'testSuite-api', type: 'test2', _connectionString: 'test2'}, function (err, datasource) {
-      expect(datasource.type).to.equal('test2');
-      done(err);
-    });
-  });
+	it("should update a data source", function (done) {
+		joola.dispatch.datasources.update({name: 'testSuite-api', type: 'test2', _connectionString: 'test2'}, function (err, datasource) {
+			expect(datasource.type).to.equal('test2');
+			done(err);
+		});
+	});
 
-  xit("should delete a data source", function (done) {
-    joola.dispatch.datasources.delete({name: 'testSuite-api'}, function (err) {
-      joolaio.dispatch.datasources.list(function (err, datasources) {
-        var exist = _.filter(datasources, function (item) {
-          return item.name == 'testSuite';
-        });
-        try {
-          expect(exist.length).to.equal(0);
-          done();
-        }
-        catch (ex) {
-          done(ex);
-        }
-      })
-    });
-  });
+	it("should delete a data source", function (done) {
+		joola.dispatch.datasources.delete({name: 'testSuite-api'}, function (err) {
+			if (err)
+				return done(err);
+			joolaio.dispatch.datasources.list(function (err, datasources) {
+				var exist = _.filter(datasources, function (item) {
+					return item.name == 'testSuite-api';
+				});
+				try {
+					expect(exist.length).to.equal(0);
+					done();
+				}
+				catch (ex) {
+					done(ex);
+				}
+			})
+		});
+	});
 });
---
+++ test/unit/2_api/users.spec.js
@@ -0,0 +1,67 @@
+/**
+ *  @title joola.io
+ *  @overview the open-source data analytics framework
+ *  @copyright Joola Smart Solutions, Ltd. <info@joo.la>
+ *  @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ **/
+
+
+describe("api-users", function () {
+	before(function (done) {
+		joola.config.clear('authentication:users:tester', function (err) {
+			if (err)
+				return done(err);
+			return done();
+		});
+	});
+
+	it("should have a valid users dispatch", function (done) {
+		expect(joola.dispatch.users).to.be.ok;
+		return done();
+	});
+
+	it("should list all available users", function (done) {
+		joola.dispatch.users.list(function (err, users) {
+			return done(err);
+		});
+	});
+
+	it("should add a user", function (done) {
+		var user = {
+			username: 'tester'
+		};
+		joola.dispatch.users.add(user, function (err, user) {
+			return done(err);
+		});
+	});
+
+	it("should fail adding a user with an already existing username", function (done) {
+		var user = {
+			username: 'tester'
+		};
+		joola.dispatch.users.add(user, function (err, user) {
+			if (err)
+				return done();
+			return done(new Error('This should fail.'));
+		});
+	});
+
+	it("should delete a user", function (done) {
+		var user = {
+			username: 'tester'
+		};
+		joola.dispatch.users.delete(user, function (err, user) {
+			if (err)
+				return done(err);
+			joola.dispatch.users.list(function (err, users) {
+				if (users[user.username])
+					return done('This should fail');
+				else
+					return done(err);
+			});
+		});
+	});
+});
