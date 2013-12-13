428f30c687533d0c27b5b294d660c7827f1396a6
itayw <itay@joo.la>
2013-12-13 11:54:11 +0200

    Fixed UUID generation function, resolves issue #74.

--- test/unit/9_common/mixin.spec.js
+++ test/unit/9_common/mixin.spec.js
@@ -10,54 +10,59 @@
  */
 
 describe("common-utils", function () {
-  it("should generate uuid - 9 chars long", function () {
-    var expected = 9;
-    var actual = joola.common.uuid().length;
-    expect(actual).to.equal(expected);
-  });
+	it("should generate uuid - 9 chars long", function () {
+		var expected = 9;
+		var actual = joola.common.uuid().length;
+		expect(actual).to.equal(expected);
+	});
 
-  it("should generate uuid - unique", function (done) {
-    var expected = joola.common.uuid();
-    var actual = joola.common.uuid();
-    if (expected == actual)
-      return done(new Error('Failed to generate a unique id [' + actual + ']:[' + expected + ']'));
-    else
-      return done();
-  });
+	it("should generate uuid - unique", function (done) {
+		var ids = [];
 
-  it("should mixin", function () {
-    var expected = {
-      test1: 1,
-      test2: 2
-    };
-    var actual = joola.common.mixin({}, expected);
-    assert(actual.test1 && actual.test2)
-  });
+		for (var i = 0; i < 100; i++) {
+			var uuid = joola.common.uuid();
+			if (ids.indexOf(uuid) > -1) {
+				return done(new Error('Found duplicate uuid [' + uuid + ']'));
+			}
+			ids.push(uuid);
+		}
 
-  it("should mixin - no overwrite", function () {
-    var expected = {
-      test1: 3,
-      test2: 2
-    };
-    var actual = joola.common.mixin({test1: 1}, expected);
-    expect(actual.test1).to.equal(1);
-  });
+		return done();
+	});
 
-  it("should mixin - overwrite", function () {
-    var expected = {
-      test1: 3,
-      test2: 2
-    };
-    var actual = joola.common.mixin({test1: 1}, expected, true);
-    expect(actual.test1).to.equal(3);
-  });
+	it("should mixin", function () {
+		var expected = {
+			test1: 1,
+			test2: 2
+		};
+		var actual = joola.common.mixin({}, expected);
+		assert(actual.test1 && actual.test2)
+	});
 
-  it("should return when add is not an object", function () {
-    var expected = {
-      test1: 3,
-      test2: 2
-    };
-    var actual = joola.common.mixin(expected);
-    expect(actual).to.equal(expected);
-  });
+	it("should mixin - no overwrite", function () {
+		var expected = {
+			test1: 3,
+			test2: 2
+		};
+		var actual = joola.common.mixin({test1: 1}, expected);
+		expect(actual.test1).to.equal(1);
+	});
+
+	it("should mixin - overwrite", function () {
+		var expected = {
+			test1: 3,
+			test2: 2
+		};
+		var actual = joola.common.mixin({test1: 1}, expected, true);
+		expect(actual.test1).to.equal(3);
+	});
+
+	it("should return when add is not an object", function () {
+		var expected = {
+			test1: 3,
+			test2: 2
+		};
+		var actual = joola.common.mixin(expected);
+		expect(actual).to.equal(expected);
+	});
 });
