5a950a67f1c379ff2ef0316a021ff706cba4b43a
itayw <itay@joo.la>
2013-12-20 09:20:57 +0200

    Adding USER object, relates to issue #91. - Added support for passing token via init (options).

--- test/unit/starthere.spec.js
+++ test/unit/starthere.spec.js
@@ -12,45 +12,62 @@
 var orig_env = process.env.NODE_ENV;
 
 before(function (done) {
-	var started = false;
+  var started = false;
 
-	try {
-		_joolaio = require('../../joola.io.js');
-		joola.state.on('state:change', function (state) {
-			if ((state == 'working' || state == 'online') && !started) {
-				started = true;
-				_sdk = require('../../lib/sdk/index');
-				var options = {
-					isBrowser: false,
-					debug: {
-						enabled: false,
-						events: {
-							enabled: false,
-							trace: false
-						},
-						functions: {
-							enabled: false
-						}
-					}
-				};
-				_sdk.init(options, function (err) {
-					if (err)
-						throw err;
-					return done();
-				});
-			}
-			else if (!started)
-				throw new Error('Failed to startup joola.io');
-		});
-	}
-	catch (ex) {
-		console.log(ex);
-		console.log(ex.stack);
-		throw ex;
-	}
+  try {
+    _joolaio = require('../../joola.io.js');
+    joola.state.on('state:change', function (state) {
+      if ((state == 'working' || state == 'online') && !started) {
+        started = true;
+
+        var testUser = {
+          username: 'test-user',
+          displayName: 'testing user',
+          _password: '1234',
+          _roles: ['admin'],
+          _filter: '',
+          organization: 'testOrg'
+        };
+
+        joola.auth.generateToken(testUser, function (err, _token) {
+          if (err)
+            throw err;
+
+          _sdk = require('../../lib/sdk/index');
+          var options = {
+            TOKEN: _token._,
+            isBrowser: false,
+            debug: {
+              enabled: false,
+              events: {
+                enabled: false,
+                trace: false
+              },
+              functions: {
+                enabled: false
+              }
+            }
+          };
+          _sdk.init(options, function (err) {
+            if (err)
+              throw err;
+
+            return done();
+          });
+        });
+      }
+      else if (!started)
+        throw new Error('Failed to startup joola.io');
+    });
+  }
+  catch (ex) {
+    console.log(ex);
+    console.log(ex.stack);
+    throw ex;
+  }
 });
 
 after(function (done) {
-	process.env.NODE_ENV = orig_env;
-	done();
+  process.env.NODE_ENV = orig_env;
+  done();
 });
