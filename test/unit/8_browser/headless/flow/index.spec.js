24d6a7266650ef48388225bb614b6f1df5378e2b
itayw <itay@joo.la>
2013-12-22 15:23:39 +0200

    Adding Login flow, relates to issue #93. - Added login form - Added bootstrap HTML5 framework - Added flow tests

---
+++ test/unit/8_browser/headless/flow/index.spec.js
@@ -0,0 +1,92 @@
+/**
+ *  joola.io
+ *
+ *  Copyright Joola Smart Solutions, Ltd. <info@joo.la>
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
+
+var
+  async = require('async'),
+  Browser = require('zombie');
+
+describe('browser-flow-login', function () {
+  before(function (done) {
+    //let's create our users
+    var calls = [];
+    console.log('aaaaa');
+    var call = function (callback) {
+      joola.config.clear('authentication:organizations:demo-org-test', callback);
+    };
+    calls.push(call);
+    var call = function (callback) {
+      joola.config.clear('authentication:users:demo-test', callback);
+    };
+    calls.push(call);
+    call = function (callback) {
+      console.log('ccccc');
+      joola.dispatch.organizations.add({name: 'demo-org-test'}, function (err) {
+        if (err)
+          console.log(err);
+        var user = {
+          username: 'demo-test',
+          displayName: 'demo test user',
+          _password: 'password',
+          _roles: ['user'],
+          _filter: '',
+          organization: 'demo-org-test'
+        };
+        console.log('ddddddd');
+        joola.dispatch.users.add(user, function (err, user) {
+          console.log(err)
+          return callback();
+        })
+      });
+
+    };
+    calls.push(call);
+    console.log('bbbbbb');
+    async.series(calls, done);
+  });
+
+  it('should load the login page when first asked for analytics', function (done) {
+    var browser = new Browser({silent: true});
+    browser.visit('http://localhost:' + joola.config.interfaces.webserver.port + '/', function () {
+      expect(browser.text("title")).to.equal('joola.io - Login');
+      expect(browser.redirected).to.equal(true);
+      expect(browser.location.href).to.equal('http://localhost:' + joola.config.interfaces.webserver.port + '/login')
+      done();
+    });
+  });
+
+  it('should decline invalid authentication', function (done) {
+    var browser = new Browser({silent: true});
+    browser.visit('http://localhost:' + joola.config.interfaces.webserver.port + '/', function () {
+      browser
+        .fill('#email', 'demo-test')
+        .fill('#password', 'password1')
+        .pressButton('Sign in', function () {
+          expect(browser.success).to.equal(true);
+          expect(browser.text("title")).to.equal('joola.io - Login');
+          done();
+        });
+    });
+  });
+
+  it('should allow valid authentication', function (done) {
+    var browser = new Browser({silent: true});
+    browser.visit('http://localhost:' + joola.config.interfaces.webserver.port + '/', function () {
+      browser
+        .fill('#email', 'demo-test')
+        .fill('#password', 'password')
+        .pressButton('Sign in', function () {
+          expect(browser.success).to.equal(true);
+          expect(browser.text("title")).to.equal('joola.io');
+          done();
+        });
+    });
+  });
+});
