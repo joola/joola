a8014ac4f700d767b4ef6c847e618490cf04fef8
itayw <itay@joo.la>
2013-12-24 17:40:10 +0200

    Added sub-menus and work on logger, relates to issue #23. - Added sub-menus to left nav bar. - Added system performance holding page - Changes to logger to support limit and sort

--- test/unit/8_browser/headless/manage/index.spec.js
+++ test/unit/8_browser/headless/manage/index.spec.js
@@ -15,7 +15,7 @@ var
 describe('browser-manage-index', function () {
 	it('should load the index page', function (done) {
 		var browser = new Browser({silent: true});
-		browser.visit("http://localhost:" + joola.config.interfaces.webserver.port + '/manage/index', function () {
+		browser.visit("http://localhost:" + joola.config.interfaces.webserver.port + '/manage/dashboard/index', function () {
 			expect(browser.text("title")).to.equal('joola.io Management Portal');
 			done();
 		});
@@ -31,7 +31,7 @@ describe('browser-manage-index', function () {
 
 	it('check that we have a valid username', function (done) {
 		var browser = new Browser({silent: true});
-		browser.visit("http://localhost:" + joola.config.interfaces.webserver.port + '/manage/index', function () {
+		browser.visit("http://localhost:" + joola.config.interfaces.webserver.port + '/manage/dashboard/index', function () {
 			var expected = 'USERNAME';
 			var actual = browser.text('.btn.dropdown-toggle');
 			expect(actual).to.equal(expected);
