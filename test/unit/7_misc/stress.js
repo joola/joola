657b2b39dc9672b9ac21daa9a9f272d47898b3e8
itayw <itay@joo.la>
2013-12-30 11:43:12 +0200

    An attempt at a small stress test for dispatch changes, relates to issue #118.

---
+++ test/unit/7_misc/stress.js
@@ -0,0 +1,51 @@
+_joolaio = require('../../../joola.io.js');
+joola.state.on('state:change', function (state) {
+  if ((state == 'working' || state == 'online')) {
+
+    var testUser = {
+      username: 'test-user',
+      displayName: 'testing user',
+      _password: '1234',
+      _roles: ['admin'],
+      _filter: '',
+      organization: 'testOrg'
+    };
+
+    joola.auth.generateToken(testUser, function (err, _token) {
+      if (err)
+        throw err;
+
+      _sdk = require('../../../lib/sdk/index');
+      var options = {
+        TOKEN: _token._,
+        isBrowser: false,
+        debug: {
+          enabled: false,
+          events: {
+            enabled: false,
+            trace: false
+          },
+          functions: {
+            enabled: false
+          }
+        }
+      };
+      _sdk.init(options, function (err) {
+        if (err)
+          throw err;
+
+        _sdk.TOKEN = _token._;
+
+        var iteration = 0;
+        setInterval(function () {
+          _sdk.dispatch.users.list(function (err, users) {
+            console.log(iteration, Object.keys(users).length);
+            iteration++;
+          },100);
+        });
+      });
+    });
+  }
+  else if (!started)
+    throw new Error('Failed to startup joola.io');
+});
