29f960c1d8fed63394bbd70b65ac0a165ed1ac82
itayw <itay@joo.la>
2014-02-10 12:32:11 +0200

    #195 added more tests - resolves #210

--- test/unit/6_query/query-basic.spec.js
+++ test/unit/6_query/query-basic.spec.js
@@ -81,4 +81,148 @@ describe("query-basic", function () {
       return done();
     });
   });
+
+  it("should perform a freestyle query [min]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', aggregation: 'min'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].values.value).to.equal(1);
+      return done();
+    });
+  });
+
+  it("should perform a freestyle query [max]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', aggregation: 'max'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].values.value).to.equal(2);
+      return done();
+    });
+  });
+  
+  it("should perform a freestyle query [name]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'Value', dependsOn: 'value', aggregation: 'avg'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.metrics[0].name).to.equal('Value');
+      return done();
+    });
+  });
+
+  it("should perform a freestyle query [prefix]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', prefix: '$'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].fvalues.value).to.equal('$3');
+      return done();
+    });
+  });
+
+  it("should perform a freestyle query [suffix]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', suffix: 'ms'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].fvalues.value).to.equal('3ms');
+      return done();
+    });
+  });
+
+  it("should perform a freestyle query [prefix+suffix]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', prefix: '$', suffix: 'ms'}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].fvalues.value).to.equal('$3ms');
+      return done();
+    });
+  });
+
+ it("should perform a freestyle query [decmials=2]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', aggregation: 'avg', decimals: 2}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].fvalues.value).to.equal('1.50');
+      return done();
+    });
+  });
+
+ it("should perform a freestyle query [decmials=0]", function (done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'minute',
+      dimensions: [],
+      metrics: [
+        {key: 'value', name: 'value', dependsOn: 'value', aggregation: 'avg', decimals: 0}
+      ]
+    };
+    joola.query.fetch(this.context, query, function (err, result) {
+      if (err)
+        return done(err);
+
+      expect(result.documents[0].fvalues.value).to.equal('2');
+      return done();
+    });
+  });
 });
