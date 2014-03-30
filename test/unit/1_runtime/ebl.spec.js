9a9b84a6f1d71f5ed337013102d189b5d41a5a82
itayw <itay@joo.la>
2014-03-30 14:23:46 +0300

    #320 Added test showingthe issue

---
+++ test/unit/1_runtime/ebl.spec.js
@@ -0,0 +1,36 @@
+describe("event loop blocks", function () {
+  before(function (done) {
+    this.context = {user: _token.user};
+    this.uid = joola.common.uuid();
+    this.workspace = _token.user.workspace;
+    this.collection = 'test-collection-ebl-' + this.uid;
+    this.documents = require('../../fixtures/basic.json');
+    this.start = new Date().getTime();
+    return done();
+  });
+
+  it("should not cause an event loop block on excessive beacon [single doc]", function (done) {
+    var self = this;
+    var requested = 1000;
+    var date = new Date().getTime();
+    var push = function (i) {
+      var doc = self.documents[0];
+      doc.timestamp = new Date(date + i);
+      joola.beacon.insert(self.context, self.context.user.workspace, self.collection, ce.clone(doc), function (err, doc) {
+        doc = doc[0];
+        expect(doc.saved).to.equal(true);
+        if (i === requested - 1)
+          done(err);
+      });
+    };
+    for (var i = 0; i < requested; i++) {
+      push(i);
+    }
+  });
+
+  after(function (done) {
+    this.end = new Date().getTime();
+    console.log('duration', this.end - this.start, 'ms');
+    done();
+  });
+});
