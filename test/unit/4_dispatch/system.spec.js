9e4d1954c570008e45896479392cd8c7c15122cf
itayw <itay@joo.la>
2014-04-02 13:19:24 +0300

    #238 coverage increase.

--- test/unit/4_dispatch/system.spec.js
+++ test/unit/4_dispatch/system.spec.js
@@ -55,6 +55,34 @@ describe("system", function () {
     });
   });
 
+  it("should blacklist an ip", function (done) {
+    joola.system.blacklist(this.context, '255.255.255.255', true, 0, function (err) {
+      if (err)
+        return done(err);
+
+      done();
+    });
+  });
+  
+  it("should remove blacklisted ip", function (done) {
+    joola.system.blacklist(this.context, '255.255.255.255', false, 0, function (err) {
+      if (err)
+        return done(err);
+
+      done();
+    });
+  });
+
+  it("should blacklist an ip with ttl", function (done) {
+    joola.system.blacklist(this.context, '255.255.255.255', true, 1000, function (err) {
+      if (err)
+        return done(err);
+
+      //TODO: Check expired
+      done();
+    });
+  });
+  
   it("should purge the cache", function (done) {
     joola.system.purgeCache(this.context, function (err) {
       if (err)
