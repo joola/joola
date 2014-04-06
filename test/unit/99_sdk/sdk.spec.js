b34a47e2cd47b2844affc2c9ae8d12bc65cdf2a6
itayw <itay@joo.la>
2014-04-06 15:42:57 +0300

    #355 Changed path names to be based on __dirname.

--- test/unit/99_sdk/sdk.spec.js
+++ test/unit/99_sdk/sdk.spec.js
@@ -1,13 +1,15 @@
 var path = require('path');
-console.log(__dirname);
+
+console.log('Running SDK dispatch tests from', path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/'));
+
 require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/collections.spec.js'));
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/config.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/logger.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/permissions.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/roles.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/system.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/users.spec.js');
-require('../../../node_modules/joola.io.sdk/test/unit/4_dispatch/workspaces.spec.js');
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/config.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/logger.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/permissions.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/roles.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/system.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/users.spec.js'));
+require(path.join(__dirname, '../../../node_modules/joola.io.sdk/test/unit/4_dispatch/workspaces.spec.js'));
 /*
  require.uncache = function (moduleName) {
  // Run over the cache looking for the files
