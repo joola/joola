f89595b58bb11fac039d222f0f8e3b4b756add32
itayw <itay@joo.la>
2014-04-09 17:53:11 +0300

    #118 Added simple benchmark flow

--- benchmark/base.bench.js
+++ benchmark/base.bench.js
@@ -1,85 +1,58 @@
-var
-  sdk = require('../node_modules/joola.io.sdk'),
-  program = require('commander'),
-  humanize = require('humanize-number');
+var common = require('../lib/common/index');
+var benchrest = require('bench-rest');
+var flows = [
+  './flows/beacon.spec.js',
+  //'./flows/beacon.large.spec.js',
+  './flows/metadata.spec.js'
+];
 
-program
-  .option('-d, --duration <n>', 'duration of test [5000]', parseInt)
-  .parse(process.argv);
+global.JOOLA_ADDRESS = 'http://127.0.0.1:8080';
 
-sdk.init({host: 'http://localhost:8080', APIToken: 'apitoken-root', debug: {enabled: false}}, function (err) {
-  if (err)
-    throw err;
+var results = {};
+results.benchmarkID = common.uuid();
+results.timestamp = new Date();
+results.flows = {};
+results.flowCount = flows.length;
 
-  console.log('joola.io ready...');
-  var n = 0;
-  var ops = 100;
-  var req = 0;
-  var errd = 0;
-  var bytes = program.size || 1024;
-  var fixtures = require('../test/fixtures/benchmark.json');
-  bytes = new Buffer(JSON.stringify(fixtures)).length;
-  var prev = start = Date.now();
-  var results = [];
-  var uid = 'TEST1';//joolaio.common.uuid();
-  console.log();
+var actual = 0;
 
-  function insert() {
-    req++;
-    joolaio.beacon.insert('benchmark-' + uid, fixtures, {}, function (err, docs) {
+var joolaio = require('joola.io.sdk');
+joolaio.init({host: 'http://localhost:8080', APIToken: 'apitoken-root', ajax: true}, function (err) {
+  joolaio.system.nodeDetails(function (err, details) {
+    if (err)
+      throw err;
+
+    results.nodeDetails = details;
+
+    flows.forEach(function (flow) {
+      console.log('Running Flow', flow);
+      var flowModule = require(flow);
+      var runOptions = flowModule.runOptions;
+      var flowExeuction = flowModule.flow;
+      var errors = [];
+
+      benchrest(flowExeuction, runOptions)
+        .on('error', function (err, ctxName) {
+          console.error('Failed in %s with err: ', ctxName, err);
+        })
+        .on('progress', function (stats, percent, concurrent, ips) {
+          console.log('Progress: %s complete', percent);
+        })
+        .on('end', function (stats, errorCount) {
+          stats.name = flowModule.name;
+          results.flows[flowModule.name] = stats;
+        });
+      if (++actual === results.flowCount)
+        return alldone();
+    });
+  });
+
+  function alldone() {
+    console.log(results);
+    joolaio.beacon.insert('benchmark', results, function (err) {
       if (err)
-        errd++;
-      if (n++ % ops == 0) {
-        var ms = Date.now() - prev;
-        var sec = ms / 1000;
-        var persec = ops / sec | 0;
-        results.push(persec);
-        process.stdout.write('\r  [' + persec + ' ops/s] [' + n + ']');
-        prev = Date.now();
-      }
-
-    });
-    setTimeout(insert, 50);
-  }
-
-  insert();
-
-  function sum(arr) {
-    return arr.reduce(function (sum, n) {
-      return sum + n;
+        throw err;
+      process.exit(0);
     });
   }
-
-  function min(arr) {
-    return arr.reduce(function (min, n) {
-      return n < min
-        ? n
-        : min;
-    });
-  }
-
-  function median(arr) {
-    arr = arr.sort();
-    return arr[arr.length / 2 | 0];
-  }
-
-  function done() {
-    var ms = Date.now() - start;
-    var avg = n / (ms / 1000);
-    console.log('\n');
-    console.log('      min: %s ops/s', humanize(min(results)));
-    console.log('     mean: %s ops/s', humanize(avg | 0));
-    console.log('   median: %s ops/s', humanize(median(results)));
-    console.log('    total: %s ops in %ds', humanize(n), ms / 1000);
-    console.log('  through: %d mb/s', ((avg * bytes) / 1024 / 1024).toFixed(2));
-    console.log('requested: %d ops', req);
-    console.log('completed: %d ops (%d%)', n, Math.round((n ) / req * 100));
-    console.log('  success: %d ops (%d%)', (n - errd), Math.round((n - errd) / n * 100));
-    console.log('  errored: %d ops', errd);
-    console.log();
-    process.exit();
-  }
-
-  process.on('SIGINT', done);
-  setTimeout(done, program.duration || 10000);
 });
---
+++ benchmark/flows/beacon.large.spec.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'beacon.large',
+  runOptions: {
+    limit: 1,         // concurrent connections
+    iterations: 1,  // number of iterations to perform
+    progress: 1000
+    //prealloc: 10     
+  },
+  flow: {
+    before: [],      // operations to do before anything
+    beforeMain: [],  // operations to do before each iteration
+    main: [  // the main flow for each iteration, #{INDEX} is unique iteration counter token
+      { post: JOOLA_ADDRESS + '/api/beacon/insert?APIToken=apitoken-root', json: require('../../test/fixtures/benchmark.json')}
+    ],
+    afterMain: [],   // operations to do after each iteration
+    after: []        // operations to do after everything is done
+  }
+};
