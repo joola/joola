a9d41219d864e1196c9ba90d887825454085d573
itayw <itay@joo.la>
2014-04-28 11:16:12 +0300

    #400 added a test to support new APITokens

--- test/unit/5_security/apitokens.spec.js
+++ test/unit/5_security/apitokens.spec.js
@@ -1,3 +1,47 @@
-/**
- * Created by itay on 4/28/14.
- */
+var
+  async = require('async');
+
+describe("security-permissions-users", function () {
+  before(function (done) {
+    var self = this;
+    this.uid = joola.common.uuid();
+
+    joolaio.options.APIToken = 'apitoken-root';
+
+    self.workspace = {
+      key: 'test-org-apitoken-' + self.uid,
+      name: 'test-org-apitoken-' + self.uid
+    };
+    self.role = {
+      key: 'test-user-role-' + self.uid,
+      _permissions: ['access_system']
+    };
+    self.user = {
+      username: 'test-user-' + self.uid,
+      _password: 'password',
+      workspace: self.workspace.key,
+      _roles: ['test-user-role-' + self.uid],
+      _APIToken: 'user-' + self.uid
+    };
+    var calls = [];
+
+    calls.push(function (callback) {
+      joolaio.workspaces.add(self.workspace, callback);
+    });
+    calls.push(function (callback) {
+      joolaio.roles.add(self.workspace.key, self.role, callback);
+    });
+    calls.push(function (callback) {
+      joolaio.users.add(self.workspace.key, self.user, callback);
+    });
+    async.series(calls, done);
+  });
+
+  it("should be able to use a newly created APIToken", function (done) {
+    var self = this;
+
+    joolaio.set('APIToken', 'user-' + self.uid, function () {
+      joolaio.system.version(done);
+    });
+  });
+});
