aa7db11ff0f419340b29bf50f6880b821bdc1b78
itayw <itay@joo.la>
2014-05-05 11:46:28 +0300

    #424 progress with API documentation - Config alignment - Added system - Added roles & permissions

--- test/unit/4_dispatch/permissions.spec.js
+++ test/unit/4_dispatch/permissions.spec.js
@@ -14,21 +14,8 @@ describe("permissions", function () {
   before(function (done) {
     this.context = {user: _token.user};
     this.uid = joola.common.uuid();
-    this.workspace= 'test-org-' + joola.common.uuid();
+    this.workspace = 'test-org-' + joola.common.uuid();
     done();
-  });
-
-  it("should add a permission", function (done) {
-    var permission = {
-      name: 'test-permission-' + this.uid
-    };
-    joola.dispatch.permissions.add(this.context, permission, function (err, _permission) {
-      if (err)
-        return done(err);
-
-      expect(_permission).to.be.ok;
-      done();
-    });
   });
 
   it("should return a valid list of permissions", function (done) {
@@ -37,93 +24,17 @@ describe("permissions", function () {
     });
   });
 
-  it("should fail adding an existing permission", function (done) {
-    var permission = {
-      name: 'test-permission-' + this.uid
-    };
-    joola.dispatch.permissions.add(this.context, permission, function (err, _permission) {
+  it("should get a permission", function (done) {
+    joola.dispatch.permissions.get(this.context, 'access_system', function (err, permissions) {
+      return done(err);
+    });
+  });
+
+  it("should not get a not existing permission", function (done) {
+    joola.dispatch.permissions.get(this.context, 'access_system2', function (err, permissions) {
       if (err)
         return done();
-
-      return done(new Error('This should fail'));
-    });
-  });
-
-
-  it("should update a permission", function (done) {
-    var permission = {
-      name: 'test-permission-' + this.uid,
-      test: 1
-    };
-    joola.dispatch.permissions.update(this.context, permission, function (err, _permission) {
-      if (err)
-        return done(err);
-      expect(_permission.test).to.equal(1);
-      done();
-    });
-  });
-
-  it("should fail updating unknown permission", function (done) {
-    var permission = {
-      name: 'test-permission1-' + this.uid,
-      test: 1
-    };
-    joola.dispatch.permissions.update(this.context, permission, function (err) {
-      if (err)
-        return done();
-
-      done(new Error('This should have failed'));
-    });
-  });
-
-  it("should fail updating permission with incomplete details", function (done) {
-    var permission = {
-
-    };
-    joola.dispatch.permissions.update(this.context, permission, function (err) {
-      if (err)
-        return done();
-
-      done(new Error('This should have failed'));
-    });
-  });
-
-  it("should delete a permission", function (done) {
-    var self = this;
-    var permission = {
-      name: 'test-permission-' + this.uid
-    };
-    joola.dispatch.permissions.delete(this.context, permission, function (err) {
-      if (err)
-        return done(err);
-
-      joola.dispatch.permissions.list(self.context, function (err, permissions) {
-        if (err)
-          return done(err);
-
-        var exist = _.filter(permissions, function (item) {
-          return item.name == 'test-permission';
-        });
-        try {
-          expect(exist.length).to.equal(0);
-          done();
-        }
-        catch (ex) {
-          done(ex);
-        }
-      });
-    });
-  });
-
-  it("should fail deleting a non existing permission", function (done) {
-    var permission = {
-      name: 'test-permission-notexist'
-    };
-    joola.dispatch.permissions.delete(this.context, permission, function (err) {
-      if (err)
-        return done();
-
-      return done(new Error('This should fail'));
+      return done(new Error('This should not fail'));
     });
   });
 });
