a757a9ba177eb9b3f4d621db12e521d47d2497dd
itayw <itay@joo.la>
2014-06-02 16:15:10 +0300

    #502 removed usage endpoint due to query ability

--- test/unit/99_REST/usage.spec.js
+++ test/unit/99_REST/usage.spec.js
@@ -12,58 +12,6 @@ describe("collections", function () {
     done();
   });
 
-  it("should report total usage for system", function (done) {
-    joolaio.system.usage(null, null, null, function (err, usage) {
-      if (err)
-        return done(err);
-
-      expect(usage).to.be.ok;
-      expect(usage.resultCount).to.be.ok;
-      expect(usage.documents).to.be.ok;
-      expect(usage.resultCount).to.be.greaterThan(0);
-      done();
-    });
-  });
-
-  it("should report total usage for workspace", function (done) {
-    joolaio.system.usage('demo', null, null, function (err, usage) {
-      if (err)
-        return done(err);
-
-      expect(usage).to.be.ok;
-      expect(usage.resultCount).to.be.ok;
-      expect(usage.documents).to.be.ok;
-      expect(usage.resultCount).to.be.greaterThan(0);
-      done();
-    });
-  });
-
-  it("should report total usage for workspace/user", function (done) {
-    joolaio.system.usage('demo', 'reader', null, function (err, usage) {
-      if (err)
-        return done(err);
-
-      expect(usage).to.be.ok;
-      expect(usage.resultCount).to.be.ok;
-      expect(usage.documents).to.be.ok;
-      expect(usage.resultCount).to.be.greaterThan(0);
-      done();
-    });
-  });
-
-  it("should report total usage for workspace/user/collection", function (done) {
-    joolaio.system.usage('demo', 'reader', 'demo-visits', function (err, usage) {
-      if (err)
-        return done(err);
-
-      expect(usage).to.be.ok;
-      expect(usage.resultCount).to.be.ok;
-      expect(usage.documents).to.be.ok;
-      expect(usage.resultCount).to.be.greaterThan(0);
-      done();
-    });
-  });
-
   it("should allow retrieval of stats on behalf of", function (done) {
     var user = {
       username: 'bypass',
@@ -84,13 +32,10 @@ describe("collections", function () {
 
         var query = {
           timeframe: 'last_year',
-          //interval: 'month',
-          //dimensions: [
-          //  {key: 'timestamp', collection: 'query'}
-          //],
           metrics: [
-            {key: 'readCount', collection: 'query'},
-            {key: 'writeCount', collection: 'beacon'}
+            {key: 'readCount', collection: 'reads'},
+            {key: 'writeCount', collection: 'writes'},
+            {key: 'simpleCount', collection: 'simple'}
           ],
           filter: []
         };
@@ -99,9 +44,7 @@ describe("collections", function () {
             return done(err);
 
           expect(usage).to.be.ok;
-          expect(usage.resultCount).to.be.ok;
-          expect(usage.documents).to.be.ok;
-          expect(usage.resultCount).to.be.greaterThan(0);
+          //expect(usage.resultCount).to.be.greaterThan(0);
           done();
         });
       });
