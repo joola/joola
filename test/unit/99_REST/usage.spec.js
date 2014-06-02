cd5176968b53df43b4cdbaa633f101267b8b00ca
itayw <itay@joo.la>
2014-06-02 15:30:27 +0300

    #502 additional tests

--- test/unit/99_REST/usage.spec.js
+++ test/unit/99_REST/usage.spec.js
@@ -12,8 +12,53 @@ describe("collections", function () {
     done();
   });
 
-  it("should report total usage", function (done) {
-    joolaio.system.usage(function (err, usage) {
+  it("should report total usage for system", function (done) {
+    joolaio.system.usage(null, null, null, function (err, usage) {
+      if (err)
+        return done(err);
+
+      expect(usage).to.be.ok;
+      expect(usage.resultCount).to.be.ok;
+      expect(usage.documents).to.be.ok;
+      expect(usage.resultCount).to.be.greaterThan(0);
+      done();
+    });
+  });
+
+  it("should report total usage for workspace", function (done) {
+    joolaio.system.usage('demo', null, null, function (err, usage) {
+      if (err)
+        return done(err);
+
+      console.log(err, usage);
+      expect(usage).to.be.ok;
+      expect(usage.resultCount).to.be.ok;
+      expect(usage.documents).to.be.ok;
+      expect(usage.resultCount).to.be.greaterThan(0);
+      done();
+    });
+  });
+
+  it("should report total usage for workspace/user", function (done) {
+    joolaio.system.usage('demo', 'reader', null, function (err, usage) {
+      if (err)
+        return done(err);
+
+      console.log(err, usage);
+      expect(usage).to.be.ok;
+      expect(usage.resultCount).to.be.ok;
+      expect(usage.documents).to.be.ok;
+      expect(usage.resultCount).to.be.greaterThan(0);
+      done();
+    });
+  });
+
+  it("should report total usage for workspace/user/collection", function (done) {
+    joolaio.system.usage('demo', 'reader', 'demo-visits', function (err, usage) {
+      if (err)
+        return done(err);
+
+      console.log(err, usage);
       expect(usage).to.be.ok;
       expect(usage.resultCount).to.be.ok;
       expect(usage.documents).to.be.ok;
