0fbf40cb544c6edfa151d94e4cca7a94c35575f8
Itay Weinberger <itay@joo.la>
2015-01-06 14:15:36 +0200

    #639 benchmark basic work

--- benchmark/flows/beacon.small.spec.js
+++ benchmark/flows/beacon.small.spec.js
@@ -1,16 +1,19 @@
 module.exports = {
-  name: 'metadata',
+  name: 'beacon',
   runOptions: {
-    limit: 500,         // concurrent connections
-    iterations:5000,  // number of iterations to perform
-    progress: 5000
-    //prealloc: 10
+    limit: 1000,         // concurrent connections
+    iterations: 15000,  // number of iterations to perform
+    progress: 5000,
+    prealloc: 1000
   },
   flow: {
     before: [],      // operations to do before anything
     beforeMain: [],  // operations to do before each iteration
     main: [  // the main flow for each iteration, #{INDEX} is unique iteration counter token
-      { get: JOOLA_ADDRESS + '/system/version?APIToken=apitoken-demo' }
+      {
+        post: JOOLA_ADDRESS + '/insert/benchmark?APIToken=apitoken-demo',
+        json: require('../../test/fixtures/benchmark-small.json')
+      }
     ],
     afterMain: [],   // operations to do after each iteration
     after: []        // operations to do after everything is done
