3879b405199c840f2ca411bf648e9dca3ea01604
Itay Weinberger <itay@joo.la>
2015-01-25 10:51:16 +0200

    #648 adding support for multi data store write.

--- test/unit/6_beacon/beacon-basic.spec.js
+++ test/unit/6_beacon/beacon-basic.spec.js
@@ -10,10 +10,9 @@ describe("beacon-basic", function () {
 
   it("should load a single document", function (done) {
     var self = this;
-    engine.beacon.insert(this.context, this.context.user.workspace, this.collection, ce.clone(this.documents[0]), function (err, doc,result) {
+    engine.beacon.insert(this.context, this.context.user.workspace, this.collection, ce.clone(this.documents[0]), function (err, doc, result) {
       if (err)
         return done(err);
-
       self.dup = new Date(doc[0].timestamp).toISOString();
       doc = doc[0];
       expect(doc.saved).to.equal(true);
