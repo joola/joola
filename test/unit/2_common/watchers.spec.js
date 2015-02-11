0255e3032d13e4154d618d8a2528ce98889b2098
Itay Weinberger <itay@joo.la>
2015-02-11 11:00:27 +0200

    #658 test coverage increase - reviewed common/watchers - reviewed common/stats - reviewed common/redis - removed unused common/modifiers - reviewed common/geoip - reviewed common/index

---
+++ test/unit/2_common/watchers.spec.js
@@ -0,0 +1,41 @@
+/**
+ *  joola
+ *
+ *  Copyright Joola Smart Solutions, Ltd. <info@joo.la>
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
+
+var sleep = require('sleep');
+
+describe("common-watchers", function () {
+  it("should monitor ELB", function (done) {
+    joola_proxy.events.once('elb', function () {
+      return done();
+    });
+    sleep.sleep(2);
+  });
+
+  it("should build EPS stats object", function (done) {
+    joola_proxy.statistics = null;
+    sleep.sleep(2);
+    joola.system.version(function (err, version) {
+      expect(joola_proxy.statistics).to.be.ok;
+      done();
+    });
+  });
+
+  it("coverage branch no redis store", function (done) {
+    joola_proxy._redis = joola_proxy.redis;
+    delete joola_proxy.redis;
+    sleep.sleep(2);
+    joola.system.version(function (err, version) {
+      joola_proxy.redis = joola_proxy._redis;
+      delete joola_proxy._redis;
+      done();
+    });
+  });
+});
