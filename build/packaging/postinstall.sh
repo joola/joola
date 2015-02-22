4647bd751830f71333a2bfe920e6f266c89948f1
Itay Weinberger <itay@joo.la>
2015-02-22 16:53:17 +0200

    #125 working on packaging.

---
+++ build/packaging/postinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+APP_NAME="joola"
+CLI="$APP_NAME"
+APP_USER=$(${CLI} config:get APP_USER)
+APP_GROUP=$(${CLI} config:get APP_GROUP)
+APP_CONFIG="/etc/${APP_NAME}/default.yml"
+
+[ -f "$APP_CONFIG" ] || cp /opt/${APP_NAME}/config/default.yml $APP_CONFIG
+chown $APP_USER.$APP_GROUP $APP_CONFIG
+ln -f -s $APP_CONFIG /opt/$APP_NAME/config/default.yml
+chmod 0640 $APP_CONFIG
+
+${CLI} scale web=1 || true
