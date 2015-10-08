08165a837d18799c30ba7a5f8d30dd487282f4f2
Itay Weinberger <itay@joo.la>
2015-10-08 11:13:31 +0300

    #748 added tests for filtered metrics

--- test/unit/7_query/query-filter.spec.js
+++ test/unit/7_query/query-filter.spec.js
@@ -1,6 +1,8 @@
-describe("query-filter", function () {
-  before(function (done) {
-    this.context = {user: _token.user};
+describe("query-filter", function() {
+  before(function(done) {
+    this.context = {
+      user: _token.user
+    };
     this.workspace = 'root';
     this.uid = global.uid;
     this.collection = 'test-collection-basic-' + this.uid;
@@ -8,7 +10,7 @@ describe("query-filter", function () {
     done();
   });
 
-  it("should query only filtered results", function (done) {
+  it("should query only filtered results", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -17,7 +19,7 @@ describe("query-filter", function () {
         ['attribute', 'eq', 'test2']
       ]
     };
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
       expect(result).to.be.ok;
@@ -29,7 +31,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query only filtered results [two conditions]", function (done) {
+  it("should query only filtered results [two conditions]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -39,7 +41,7 @@ describe("query-filter", function () {
         ['attribute2', 'eq', 'test22'],
       ]
     };
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
       expect(result).to.be.ok;
@@ -51,7 +53,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query only filtered results [two conditions, no results]", function (done) {
+  it("should query only filtered results [two conditions, no results]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -61,7 +63,7 @@ describe("query-filter", function () {
         ['attribute2', 'eq', 'test222'],
       ]
     };
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
       expect(result).to.be.ok;
@@ -74,7 +76,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query only filtered results [regex]", function (done) {
+  it("should query only filtered results [regex]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -83,7 +85,7 @@ describe("query-filter", function () {
         ['attribute', 'regex', 'test.*?']
       ]
     };
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
 
@@ -96,7 +98,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query for in-array values", function (done) {
+  it("should query for in-array values", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -105,7 +107,7 @@ describe("query-filter", function () {
         ['interests', 'in', '[1,2]']
       ]
     };
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
 
@@ -118,7 +120,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query for in-array values [strict, ES only]", function (done) {
+  it("should query for in-array values [strict, ES only]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -131,7 +133,7 @@ describe("query-filter", function () {
     if (joola_proxy.datastore.providers.default.name !== 'ElasticSearch')
       return done();
 
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
 
@@ -144,7 +146,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query for ranges [gt, ES only]", function (done) {
+  it("should query for ranges [gt, ES only]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -157,7 +159,7 @@ describe("query-filter", function () {
     if (joola_proxy.datastore.providers.default.name !== 'ElasticSearch')
       return done();
 
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
 
@@ -170,7 +172,7 @@ describe("query-filter", function () {
     });
   });
 
-  it("should query for ranges [gte, ES only]", function (done) {
+  it("should query for ranges [gte, ES only]", function(done) {
     var query = {
       dimensions: [],
       metrics: ['value', 'another'],
@@ -183,7 +185,7 @@ describe("query-filter", function () {
     if (joola_proxy.datastore.providers.default.name !== 'ElasticSearch')
       return done();
 
-    joola_proxy.query.fetch(this.context, query, function (err, result) {
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
       if (err)
         return done(err);
 
@@ -195,4 +197,146 @@ describe("query-filter", function () {
       return done();
     });
   });
+
+  it("should perform a freestyle sum w/ filter", function(done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'day',
+      dimensions: [],
+      metrics: [{
+        key: 'value',
+        filter: [
+          ['attribute', 'eq', 'test2']
+        ]
+      }],
+      collection: this.collection
+    };
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
+      if (err)
+        return done(err);
+      expect(result).to.be.ok;
+      expect(result.documents).to.be.ok;
+      expect(result.documents.length).to.be.greaterThan(0);
+      expect(result.documents[0].value).to.equal(2);
+      return done();
+    });
+  });
+
+  it("should perform a freestyle sum w/ 2 metrics, 1 filtered", function(done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'day',
+      dimensions: [],
+      metrics: [{
+        key: 'value',
+        filter: [
+          ['attribute', 'eq', 'test2']
+        ]
+      }, 'another'],
+      collection: this.collection
+    };
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
+      if (err)
+        return done(err);
+      expect(result).to.be.ok;
+      expect(result.documents).to.be.ok;
+      expect(result.documents.length).to.be.greaterThan(0);
+      expect(result.documents[0].value).to.equal(2);
+      expect(result.documents[0].another).to.equal(30);
+      return done();
+    });
+  });
+
+  it("should perform a freestyle sum w/ 2 metrics, 2 filtered", function(done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'day',
+      dimensions: [],
+      metrics: [{
+        key: 'value',
+        filter: [
+          ['attribute', 'eq', 'test2']
+        ]
+      }, {
+        key: 'another',
+        filter: [
+          ['attribute', 'eq', 'test2']
+        ]
+      }],
+      collection: this.collection
+    };
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
+      if (err)
+        return done(err);
+      expect(result).to.be.ok;
+      expect(result.documents).to.be.ok;
+      expect(result.documents.length).to.be.greaterThan(0);
+      expect(result.documents[0].value).to.equal(2);
+      expect(result.documents[0].another).to.equal(20);
+      return done();
+    });
+  });
+
+  it("should perform a freestyle sum w/ 2 metrics, 1 filtered, general filter", function(done) {
+    var query = {
+      timeframe: 'last_day',
+      interval: 'day',
+      dimensions: [],
+      metrics: [{
+        key: 'value',
+        filter: [
+          ['attribute', 'eq', 'test2']
+        ]
+      }, 'another'],
+      filter: [
+        ['tags', '_in', '["book"]']
+      ],
+      collection: this.collection
+    };
+    joola_proxy.query.fetch(this.context, query, function(err, result) {
+      if (err)
+        return done(err);
+      expect(result).to.be.ok;
+      expect(result.documents).to.be.ok;
+      expect(result.documents.length).to.be.greaterThan(0);
+      expect(result.documents[0].value).to.equal(2);
+      expect(result.documents[0].another).to.equal(20);
+      return done();
+    });
+  });
+
+it("should perform a freestyle calculated metric w/ 2 metrics, 2 filtered", function(done) {
+  var query = {
+    timeframe: 'last_day',
+    interval: 'day',
+    dimensions: [],
+    metrics: [{
+      key:'calcvalue',
+      formula:{
+        dependsOn:[{
+          key: 'value',
+          filter: [
+            ['attribute', 'eq', 'test2']
+          ]
+        }, {
+          key: 'another',
+          filter: [
+            ['attribute', 'eq', 'test2']
+          ]
+        }],
+        run: 'function(value, another) { return value * another }'
+      }
+    }],
+    collection: this.collection
+  };
+  joola_proxy.query.fetch(this.context, query, function(err, result) {
+    if (err)
+      return done(err);
+    expect(result).to.be.ok;
+    expect(result.documents).to.be.ok;
+    expect(result.documents.length).to.be.greaterThan(0);
+    expect(result.documents[0].calcvalue).to.equal(40);
+    return done();
+  });
+});
 });
